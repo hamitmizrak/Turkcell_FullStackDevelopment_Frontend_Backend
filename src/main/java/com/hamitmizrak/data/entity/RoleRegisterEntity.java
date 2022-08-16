449068cca8c89963ce907a9b04a2eb1f62ddd956
hamitmizrak <hamitmizrak@gmail.com>
2022-08-16 10:52:37 +0300

    RoleEntity ve UserRegisterEntity eklendi

---
+++ src/main/java/com/hamitmizrak/data/entity/RoleRegisterEntity.java
@@ -0,0 +1,52 @@
+package com.hamitmizrak.data.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+
+//Lombok
+@Data
+@Builder
+
+//Entity
+@Entity
+@Table(name="role_register")
+public class RoleRegisterEntity  implements Serializable {
+    public static final Long serialVersionUID =1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="roles_id")
+    private Long rolesId;
+
+    @Column(name="role_name")
+    private String roleName;
+
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    //Relation :
+    // UserRegisterEntity (EAGER)
+    // RoleRegisterEntity (LAZY)
+    @ManyToMany( mappedBy = "relationRoleRegisterEntities" ,fetch = FetchType.LAZY)
+    private Collection<UserRegisterEntity> userRegisterEntities;
+
+    //parametresiz constructor:
+    public RoleRegisterEntity(){
+    }
+
+    //parametreli constructor:  Dikkat userRegisterEntities ekledim
+    public RoleRegisterEntity(Long rolesId, String roleName, Date createdDate, Collection<UserRegisterEntity> userRegisterEntities) {
+        this.rolesId = rolesId;
+        this.roleName = roleName;
+        this.createdDate = createdDate;
+        this.userRegisterEntities = userRegisterEntities;
+    }
+}
---
+++ src/main/java/com/hamitmizrak/data/entity/UserRegisterEntity.java
@@ -0,0 +1,59 @@
+package com.hamitmizrak.data.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+
+//Lombok
+@Data
+@Builder
+
+//Entity
+@Entity
+@Table(name="register")
+public class UserRegisterEntity implements Serializable {
+    public static final Long serialVersionUID =1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_register_id")
+    private Long userRegisterId;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String picture;
+    private boolean isLogin;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    //Relation : Register burası Eager
+    @ManyToMany(fetch = FetchType.EAGER)
+    //burarada UserRegisterEntity ve RoleRegisterEntity id bağlama işlemini yaptım
+    //inverseJoinColumns ==> karşımızdaki RoleRegisterEntity @Column name verdik
+    @JoinTable(joinColumns =@JoinColumn(name="user_register_id") ,inverseJoinColumns = @JoinColumn(name="roles_id"))
+    private Collection<RoleRegisterEntity> relationRoleRegisterEntities;
+
+    //parametresiz constructor:
+    public UserRegisterEntity(){
+    }
+
+    //parametreli constructor:  Dikkat RoleRegisterEntity ekledim
+    public UserRegisterEntity(Long userRegisterId, String name, String surname, String email, String password, String picture, boolean isLogin, Date createdDate, Collection<RoleRegisterEntity> relationRoleRegisterEntities) {
+        this.userRegisterId = userRegisterId;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.picture = picture;
+        this.isLogin = isLogin;
+        this.createdDate = createdDate;
+        this.relationRoleRegisterEntities = relationRoleRegisterEntities;
+    }
+}
