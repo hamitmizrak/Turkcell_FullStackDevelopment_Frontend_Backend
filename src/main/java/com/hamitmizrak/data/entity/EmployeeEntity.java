3125e390e7d59290bbc9b96a1562ecfca2c2ff8e
hamitmizrak <hamitmizrak@gmail.com>
2022-08-16 10:52:53 +0300

    ana dizin

--- src/main/java/com/hamitmizrak/data/entity/EmployeeEntity.java
+++ src/main/java/com/hamitmizrak/data/entity/EmployeeEntity.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 //lombok
 @Data
@@ -20,7 +21,8 @@ import javax.persistence.Table;
 //Entity
 @Entity
 @Table(name="employee")
-public class EmployeeEntity  extends  BaseEntity{
+public class EmployeeEntity  extends  BaseEntity implements Serializable {
+    public static final Long serialVersionUID =1L;
 
     private String  name;
     private String  surname;
--- src/test/java/com/hamitmizrak/TurkcellFullStackDevelopmentApplicationTests.java
+++ src/test/java/com/hamitmizrak/TurkcellFullStackDevelopmentApplicationTests.java
@@ -19,88 +19,96 @@ import static org.junit.jupiter.api.Assertions.*;
 //test: birbirinden bağımsız yapılar için kullanıyoruz.
 @SpringBootTest
 @Log4j2
-class TurkcellFullStackDevelopmentApplicationTests implements ITest {
-
-    @Autowired
-    IEmployeeRepository repository;
 
 
-    @BeforeAll
-    static void getBeforeAllMethod(){
-        System.out.println("Bütün testtelerden önce gelirim");
-        log.error("Bütün testtelerden önce gelirim");
-    }
 
-    @BeforeEach
-    void getPreviousEmployeeBeforeEach(){
-        System.out.println("Employee Testten hemen öncesi");
-        log.error("Bütün testtelerden önce gelirim");
-    }
-
-    @Test
-    @Override
-    //testin amaçı: bu test database veri ekleyebiliyor mu ?
-    public void testSave() {
-        EmployeeEntity employeeEntity=EmployeeEntity.builder().name("hamit").surname("mızrak").price(1).hescode("hescode").build();
-        repository.save(employeeEntity);
-        //Test : eklenenen ik veriyi sistemde göstermek
-        //eğer olmazsa yandaki hatayı döner: java.util.NoSuchElementException: No value present
-        assertNotNull(repository.findById(6L).get());
-    }
-
-
-    @Test
-    @Override
-    //Biraz önce yazdığımız testSave bulabiliyor muyuz
-    public void testFind() {
-        EmployeeEntity employeeFind= repository.findById(6L).orElseThrow(()->new ResourceNotFoundException("id Bulunamadı"));
-        //eğer olmazsa yandaki hatayı döner: java.util.NoSuchElementException: No value present
-        //expected: Beklenen
-        //actual: Gerçekte olan
-        //assertEquals(short expected, short actual)
-        //eğer beklenen ile gerçekte olan aynı ise
-        // başarılıdır(Success)
-        // değilse ==> Failed
-        assertEquals("hamit",employeeFind.getName());
-    }
-
-
-    @Test
-    @Override
-    public void testList() {
-        List<EmployeeEntity> list = repository.findAll();
-        assertThat(list).size().isGreaterThan(0);
-    }
-
-
-    @Test
-    @Override
-    public void testUpdate() {
-        EmployeeEntity employeeFind= repository.findById(1L).orElseThrow(()->new ResourceNotFoundException("id Bulunamadı"));
-        employeeFind.setName("Malatya");
-        repository.save(employeeFind);
-
-        assertNotEquals("hamit",repository.findById(1L).get().getName());
-    }
-
-
-    @Test
-    @Override
-    public void testDelete() {
-        repository.deleteById(1L);
-        assertThat(repository.existsById(1L)).isFalse();
-    }
-
-    @AfterEach
-    void getPreviousEmployeeAftereEach(){
-        System.out.println("Employee Testten hemen sonrasında");
-        log.error("Employee Testten hemen sonrasında");
-    }
-
-    @AfterAll
-    static void getAfterAllMethod(){
-        System.out.println("Employee Bütün testlerden hemen sonra  gelirim");
-        log.error("Employee Bütün testlerden hemen sonra  gelirim");
-    }
+class TurkcellFullStackDevelopmentApplicationTests {
 
 }
+
+
+//class TurkcellFullStackDevelopmentApplicationTests implements ITest {
+//
+//    @Autowired
+//    IEmployeeRepository repository;
+//
+//
+//    @BeforeAll
+//    static void getBeforeAllMethod(){
+//        System.out.println("Bütün testtelerden önce gelirim");
+//        log.error("Bütün testtelerden önce gelirim");
+//    }
+//
+//    @BeforeEach
+//    void getPreviousEmployeeBeforeEach(){
+//        System.out.println("Employee Testten hemen öncesi");
+//        log.error("Bütün testtelerden önce gelirim");
+//    }
+//
+//    @Test
+//    @Override
+//    //testin amaçı: bu test database veri ekleyebiliyor mu ?
+//    public void testSave() {
+//        EmployeeEntity employeeEntity=EmployeeEntity.builder().name("hamit").surname("mızrak").price(1).hescode("hescode").build();
+//        repository.save(employeeEntity);
+//        //Test : eklenenen ik veriyi sistemde göstermek
+//        //eğer olmazsa yandaki hatayı döner: java.util.NoSuchElementException: No value present
+//        assertNotNull(repository.findById(6L).get());
+//    }
+//
+//
+//    @Test
+//    @Override
+//    //Biraz önce yazdığımız testSave bulabiliyor muyuz
+//    public void testFind() {
+//        EmployeeEntity employeeFind= repository.findById(6L).orElseThrow(()->new ResourceNotFoundException("id Bulunamadı"));
+//        //eğer olmazsa yandaki hatayı döner: java.util.NoSuchElementException: No value present
+//        //expected: Beklenen
+//        //actual: Gerçekte olan
+//        //assertEquals(short expected, short actual)
+//        //eğer beklenen ile gerçekte olan aynı ise
+//        // başarılıdır(Success)
+//        // değilse ==> Failed
+//        assertEquals("hamit",employeeFind.getName());
+//    }
+//
+//
+//    @Test
+//    @Override
+//    public void testList() {
+//        List<EmployeeEntity> list = repository.findAll();
+//        assertThat(list).size().isGreaterThan(0);
+//    }
+//
+//
+//    @Test
+//    @Override
+//    public void testUpdate() {
+//        EmployeeEntity employeeFind= repository.findById(1L).orElseThrow(()->new ResourceNotFoundException("id Bulunamadı"));
+//        employeeFind.setName("Malatya");
+//        repository.save(employeeFind);
+//
+//        assertNotEquals("hamit",repository.findById(1L).get().getName());
+//    }
+//
+//
+//    @Test
+//    @Override
+//    public void testDelete() {
+//        repository.deleteById(1L);
+//        assertThat(repository.existsById(1L)).isFalse();
+//    }
+//
+//    @AfterEach
+//    void getPreviousEmployeeAftereEach(){
+//        System.out.println("Employee Testten hemen sonrasında");
+//        log.error("Employee Testten hemen sonrasında");
+//    }
+//
+//    @AfterAll
+//    static void getAfterAllMethod(){
+//        System.out.println("Employee Bütün testlerden hemen sonra  gelirim");
+//        log.error("Employee Bütün testlerden hemen sonra  gelirim");
+//    }
+//
+//}
